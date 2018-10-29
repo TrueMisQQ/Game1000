0aefed164a78515ad4116d671f2785647469bf31
TrueMisQQ <misq92@gmail.com>
2018-10-29 15:26:28 +0100

    Style Change, DiceSpawning
    zmiana stylu na czarnu, dodanie prymitywnej metody tworzenia kostek w grze.

---
+++ Game1000v1.1/Assets/Scripts/DiceSpawnScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceSpawnScript : MonoBehaviour
+
+{
+    
+    public GameObject DiceObject;
+    private bool isThrown = false;
+
+    public void DiceThrow()
+    {
+        if (isThrown == false) {
+            for (int i =0 ; i < 5; i++)
+        {
+            Instantiate(DiceObject, transform.position, transform.rotation);
+
+                
+                
+        }
+            isThrown = true;
+        }
+
+        else
+        {
+            isThrown = false;
+};
+    }
+
+
+}
+
